1f1f117821cb6419667945adb37ef86f3dbc2f6d
Archish Mehta <archishhmehta@gmail.com>
2025-06-15 11:52:02 -0400

    Add files via upload
    a few updates

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
 function toggleMenu() {
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
-
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
-
-    
-    if (menu.classList.contains('open')) {
-        menu.style.height = `${menu.scrollHeight}px`;
-    } else {
-        menu.style.height = '0';
-    }
+    menu.classList.toggle("open")
+    icon.classList.toggle("open")
 }
